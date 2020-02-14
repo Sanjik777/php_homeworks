098b30c3ba92751f1e07bd7dae0a63d5575c926f
Sanjik777 <40425088+Sanjik777@users.noreply.github.com>
2020-02-14 20:00:15 +0600

    Add files via upload

--- lessons.loc/index.php
+++ lessons.loc/index.php
@@ -7,9 +7,26 @@ $html=Tag::html();
 $head=Tag::head()->appendTo($html);
 $body=Tag::body()->appendTo($html);
 
-$container= Tag::div()->appendBody($body);
+$container= Tag::div()->appendTo($body);
 $container->setAttributes('class','container');
+
+$link=new Link();
+$link
+    ->appendBody('Hello')
+    //->setAttributes('href','http://google.com');
+        //для короткости создадим ф-ию href  вклассе
+    ->href('http://google.com');
+$container->appendBody($link);
+
 echo $html;
+
+
+
+
+
+
+
+
 
 
 
